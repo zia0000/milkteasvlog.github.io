0f2fd9de5e1a8839f9bb5728ed334df242b6a462
Gia <kgkim774@gmail.com>
2022-03-02 19:59:03 +0900

    emojichanged

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ posts }) {
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
           {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            🧑🏻‍💻 Latest
+            🗽 Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
